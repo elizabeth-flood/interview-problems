63123825531048260ba6a69773c23d6f005ef975
elizabeth-flood <emc0094@gmail.com>
2021-03-21 14:19:47 -0600

    Create StringRotation.java

---
+++ cracking-the-coding-interview/arrays-strings/StringRotation.java
@@ -0,0 +1,14 @@
+
+/**
+ * 
+ * Problem: Assume you have a method isSubstring which checks if one word is a
+ * substring of another. Given two Strings, check if s2 is a rotation of s1
+ * using only one call to isSubstring
+ *
+ */
+public class StringRotation {
+  
+    public static void main (String[] args) {
+	    System.out.println ("Hello World!");
+    }
+}
