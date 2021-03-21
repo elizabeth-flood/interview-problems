3ddce4474481fad29835d51f9b90befb4914c352
elizabeth-flood <emc0094@gmail.com>
2021-03-21 15:15:58 -0600

    Update IsUnique.java

--- cracking-the-coding-interview/arrays-strings/IsUnique.java
+++ cracking-the-coding-interview/arrays-strings/IsUnique.java
@@ -1,3 +1,4 @@
+import java.util.*;
 /**
  * 
  * Problem: Implement an algorithm to determine if a string has all unique
@@ -7,12 +8,25 @@
 public class IsUnique {
 	
 	public boolean isUnique(String input) {
-		return true;
+		// string to string array to walk through each
+		// each char has a value 
+		// no extra data structure means do this on the fly
+		
+		// I don't know how to do with without an extra data structure, lets get something working
+		// iterate string as array, add char to a set, check if set is same length as array, O(n + n) data storage, O(n) pass through list once and then one lookup
+		
+		Set<Character> uniqueItems = new HashSet<>();
+		// O(n)
+		for (int i = 0; i < input.length(); i++) {
+			uniqueItems.add(new Character(input.charAt(i)));
+		}
+		
+		return uniqueItems.size() == input.length(); // O(n)
 	}
 	
     	public static void main (String[] args) {
 		IsUnique iu = new IsUnique();
-		String passInput = "alunique";
+		String passInput = "aluniqe";
 		String failInput = "allunique";
 	    
 		System.out.println(iu.isUnique(passInput));
@@ -20,3 +34,12 @@ public class IsUnique {
 	}
 
 }
+
+// things I needed to Google:
+// string to string array -> split("") 
+// how to add to set
+// how to format foreach
+// how to do equals
+// char vs Character
+// toCharArray() and its runtime
+// charAt
