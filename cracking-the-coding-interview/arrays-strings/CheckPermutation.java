90f57ec075c740432e458e79dfe7aac28d22544a
elizabeth-flood <emc0094@gmail.com>
2021-03-21 14:16:07 -0600

    Create CheckPermutation.java

---
+++ cracking-the-coding-interview/arrays-strings/CheckPermutation.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ * Problem: Given two strings, write a method to decide if one is a permutation
+ * of other.
+ *
+ * Solution: clarify case sensitive and whitespace
+ *
+ */
+public class CheckPermutation {
