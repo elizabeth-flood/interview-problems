36fc76a87b25ad6b1e39396ff2b6f5e22f18a376
elizabeth-flood <emc0094@gmail.com>
2021-03-21 14:20:06 -0600

    Create Urlify.java

---
+++ cracking-the-coding-interview/arrays-strings/Urlify.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ * Problem: Write a method to replace all spaces in a string with '%20 You may
+ * assume that the string has sufficient space at the end to hold the additional
+ * characters, and that you are given the "true" length of the string. (Note: if
+ * implementing in Java, please use a character array so that you can perform
+ * this operation in place.) EXAMPLE Input: "Mr John Smith     ", 13 Output:
+ * "Mr%20John%20Smith"
+ * 
+ * Solution: Start from the end and work backwards
+ *
+ */
+public class Urlify {
+  
+    public static void main (String[] args) {
+	    System.out.println ("Hello World!");
+    }
+}
