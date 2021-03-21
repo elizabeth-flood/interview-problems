749a67c376edaaf8a4acd1a46a338d3ad7d59310
elizabeth-flood <emc0094@gmail.com>
2021-03-21 14:50:02 -0600

    Update IsUnique.java

--- cracking-the-coding-interview/arrays-strings/IsUnique.java
+++ cracking-the-coding-interview/arrays-strings/IsUnique.java
@@ -5,8 +5,18 @@
  *
  */
 public class IsUnique {
+	
+	public boolean isUnique(String input) {
+		return true;
+	}
+	
+    	public static void main (String[] args) {
+		IsUnique iu = new IsUnique();
+		String passInput = "alunique";
+		String failInput = "allunique";
+	    
+		System.out.println(iu.isUnique(passInput));
+		System.out.println(iu.isUnique(failInput));
+	}
 
-    public static void main (String[] args) {
-	    System.out.println ("Hello World!");
-    }
 }
