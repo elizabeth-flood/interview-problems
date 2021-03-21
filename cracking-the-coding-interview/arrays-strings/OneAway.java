ddeb699f263eac414d02144892c35679f16c5b3e
elizabeth-flood <emc0094@gmail.com>
2021-03-21 14:17:41 -0600

    Create OneAway.java

---
+++ cracking-the-coding-interview/arrays-strings/OneAway.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ * Problem: One Away: There are three types of edits that can be performed on
+ * strings: insert a character, remove a character, or replace a character.
+ * Given two strings, write a function to check if they are one edit (or zero
+ * edits) away.
+ * 
+ * Solution:
+ *
+ */
+public class OneAway {
+  
+      public static void main (String[] args) {
+	    System.out.println ("Hello World!");
+    }
+}
