51f9cedc3cd1d9895e9218332c736fe51051d7c3
elizabeth-flood <emc0094@gmail.com>
2021-03-21 14:18:21 -0600

    Create PalindromePermutation.java.java

---
+++ cracking-the-coding-interview/arrays-strings/PalindromePermutation.java.java
@@ -0,0 +1,17 @@
+
+/**
+ * 
+ * Problem: Palindrome Permutation: Given a string, write a function to check if
+ * it is a permutation of a palindrome. A palindrome is a word or phrase that is
+ * the same forwards and backwards. A permutation is a rearrangement of letters.
+ * The palindrome does not need to be limited to just dictionary words.
+ * 
+ * EXAMPLE Input: tact coa Output: True (permutations: "taco cat'; "atco eta·;
+ * etc.)
+ *
+ */
+public class PalindromePermutation {
+    public static void main (String[] args) {
+	    System.out.println ("Hello World!");
+    }
+}
